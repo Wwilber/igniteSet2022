2dd6001b344384c3b1a9f604413b3691df6d137e
xxx <93487277+Wwilber@users.noreply.github.com>
2022-10-07 11:33:56 -0300

    finalizada e testada os parametros do ambiente mobile

--- esports/mobile/App.tsx
+++ esports/mobile/App.tsx
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+interface ButtonProps{
+  title: string;
+}
+
+function Button(props: ButtonProps){
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>WILBER RIBEIRO!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        WILBER RIBEIRO REACT NATIVE!
+      </Text>
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 4" />
+
+      <StatusBar style="auto"/>
     </View>
   );
 }
 
+
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
